26446dda9a19c0286fbdf750e3154d9280a3af38
Ali mahjoob <behnamuiux@gmail.com>
2026-07-19 01:38:51 +0330

    edit docker

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -8,10 +8,7 @@ import io.ktor.server.engine.embeddedServer
 
 fun main(args: Array<String>) {
     io.ktor.server.cio.EngineMain.main(args)
-    val port = System.getenv("PORT")?.toInt() ?: 8080
 
-    embeddedServer(CIO, port = port, host = "0.0.0.0", module = Application::module)
-        .start(wait = true)
 
 
 
